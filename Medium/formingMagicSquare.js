77ea536554e40da2fd0d4cc9c94df08293c2ca06
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2024-01-18 00:31:14 +0300

    18.01.2024

---
+++ Medium/formingMagicSquare.js
@@ -0,0 +1,6 @@
+// A Magic Square is a n x n matrix of distinct element from 
+//1 to n2 where the sum of any row, column or diagonal is always equal to same number. 
+// Consider a 3 X 3 matrix, s, of integers in the inclusive range [1, 9] .
+// We can convert any digit, a, to any other digit, b, in the range [1, 9] at cost |a – b|. 
+// Given s, convert it into a magic square at minimal cost by
+// changing zero or more of its digits. The task is to find minimum cost. 
