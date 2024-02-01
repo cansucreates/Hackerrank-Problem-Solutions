55bad14bc6e86b4baa26ddb96430503217dd5531
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-02-01 23:42:16 +0300

    1.2.24

---
+++ Easy/BeautifulDays.js
@@ -0,0 +1,32 @@
+function beautifulDays(i,j,k) {
+
+//the reverser function
+function reverseNumber(num) {
+    let strNum = num.toString();
+    let reversed = strNum.split("").reverse().join("");
+    return parseInt(reversed);
+}
+
+// for to find the numbers between i and j
+function arrayRange(i,j) {
+    let arr = [];
+    for (let x = i; x <= j; x++) {
+        arr.push(x);
+    }
+    return arr; }
+
+ // the main function
+ 
+ let beautifulDaysCounter = 0;
+
+ let theDays = arrayRange(i,j);
+
+ for(let i = 0; i < theDays.length; i++) {
+     if((theDays[i] - reverseNumber(theDays[i])) % k === 0) {
+         beautifulDaysCounter++;
+     }
+ }
+
+    return beautifulDaysCounter;
+
+}
