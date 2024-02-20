2cbadaf709e40e4ed6b2012f01b3dd2ff3540f32
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-02-20 23:18:05 +0300

    20.01.24

---
+++ Easy/appendAndDelete.js
@@ -0,0 +1,9 @@
+// Iterate through both strings and find the index i where they differ.
+// Calculate the number of operations needed to delete the characters from index i to the end of s, 
+// and to append the characters from index i to the end of t.
+// Compare this total number of operations with k to determine 
+// if it's possible within the given limit.
+
+function appendAndDelete(s, t, k) {
+    
+}
