1d9d221cb3d366435b2fa8c92d1b13814fbb8253
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-02-21 16:45:59 +0300

    21.02.24

--- Easy/appendAndDelete.js
+++ Easy/appendAndDelete.js
@@ -1,9 +1,13 @@
-// Iterate through both strings and find the index i where they differ.
-// Calculate the number of operations needed to delete the characters from index i to the end of s, 
-// and to append the characters from index i to the end of t.
-// Compare this total number of operations with k to determine 
-// if it's possible within the given limit.
-
 function appendAndDelete(s, t, k) {
+
+    if(k >= (s.length + t.length)) return 'Yes' // if k is greater than the sum of the lengths of s and t, we can delete all of s and append all of t
     
+    let i = 0 // count the number of characters that are the same in both strings
+    while(s[i]==t[i] && i < s.length && i<t.length){
+        i++
+    }
+    const rest = (s.length - i)+(t.length - i) // the number of characters that need to be deleted from s and appended to t
+    if(k<rest) return 'No' // if k is less than the number of characters that need to be deleted and appended, we can't do it
+    else if(rest === k || ((k - rest)%2)==0) return 'Yes' // if k is equal to the number of characters that need to be deleted and appended, or if k is even and greater than the number of characters that need to be deleted and appended, we can do it
+    else return 'No' // 
 }
