4bb9258bedf81aec4b3f33a195863e36a879ffdc
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-12-09 23:39:06 +0300

    9.12.24

--- Medium/nonDivisibleSubset.js
+++ Medium/nonDivisibleSubset.js
@@ -1,21 +1,36 @@
 // Given a set of distinct integers, print the size of a maximal subset of S where the sum of any 2 numbers in S' is not evenly divisible by k.
 
 function nonDivisibleSubset(k, s) {
-    let count = Array(k).fill(0);
-    s.forEach(num => {
-        count[num % k]++;
-    });
-    
-    let result = Math.min(count[0], 1);
-    for (let i = 1; i <= k / 2; i++) {
-        if (i !== k - i) {
-            result += Math.max(count[i], count[k - i]);
-        }
-    }
-    if (k % 2 === 0) {
-        result++;
-    }
-    
-    return result;
+    // her kalanı saymak için array oluştur
+    let remainders = new Array(k).fill(0);
 
+    // her sayının kalanını hesaplayıp say
+    for (let sayi of s) {
+        remainders[sayi%k]++; // kalanı her defasında arttır.
+    }
+
+    let max = 0;
+
+    // eğer kalan 0 ise
+    if (remainders[0] > 0) {
+        max = 1;
+    }
+
+    // kalanları k'nın yarısı kadar check et tamamlayacıları ile karşılaştır için.
+    for (let i = 1; i <= k/2; i ++ ) {
+        // normal durumlar için
+        if (i !== k-i) {
+            max += Math.max(remainders[i], remainders[k-i]);
+        }
+        else {
+            // eğer i === k-i ise 
+            if (remainders[i] > 0) 
+            {
+                max += 1;
+            }
+            
+        }
+
+    }
+    return max;
 }
